71570fd84e8daf1bd85fbe74e29acb88c7f302fc
MEugeV <eugeniavogt@yahoo.com.ar>
2025-04-18 20:01:50 -0300

    Evito que rompan valores no enteros y reconsulto en seleccion de método si no es correcto

--- TP-Mate-Prog.py
+++ TP-Mate-Prog.py
@@ -1,27 +1,38 @@
 """ Este programa sirve para simular compuertas AND, OR, NOT, NAND, NOR Y XOR """
 
-## Definimos las funciones que usaremos para evaluar las funciones booleanas
+# Definimos las funciones que usaremos para evaluar las funciones booleanas
+
 
 def pedir_valor_bool(mensaje):
     while True:
-        valor = int(input(mensaje))
-        if valor in (0,1):
-            return valor
+        valor = input(mensaje)
+        if valor in ("0", "1"):
+            return int(valor)
         else:
-            print("Valor incorrecto. Los valores booleanos son 1 o 0. Intente nuevamente: ")
-            valor
+            print(
+                "Valor incorrecto. Los valores booleanos son 1 o 0. Intente nuevamente: ")
 
 
-def funcionAnd (x,y):
-    if x and y == 1 :
-        return True    
+def pedir_compuerta():
+    message = "Elija que función desea analizar: \n 1- Funcion AND\n 2- Funcion OR\n 3- Funcion NOT\n 4- Funcion NAND\n 5- Funcion NOR \n 6- Funcion NOR\n Su eleccion: "
+    while True:
+        valor = input(message)
+        if valor in ("0", "1", "2", "3", "4", "5", "6"):
+            return int(valor)
+        else:
+            print("El valor ingresado no es una función válida. Intente nuevamente: ")
+
+
+def funcionAnd(x, y):
+    if x and y == 1:
+        return True
     else:
         return False
 
 
-def funcionOr (x,y):
-    if x or y == 1 :
-        return True    
+def funcionOr(x, y):
+    if x or y == 1:
+        return True
     else:
         return False
 
@@ -33,7 +44,7 @@ def funcionNot(x):
         return 1
 
 
-## Se pide al usuario ingresar valores binarios
+# Se pide al usuario ingresar valores binarios
 
 valorA = pedir_valor_bool("Inserte un valor booleano: ")
 print(f"El primer valor ingresado es: {valorA}")
@@ -42,71 +53,71 @@ valorB = pedir_valor_bool("Inserte otro valor booleano: ")
 print(f"El segundo valor ingresado es: {valorB}")
 
 
-## Creamos un pequeño menu para pedirle al usuario que funcion quiere usar
+# Creamos un pequeño menu para pedirle al usuario que funcion quiere usar
 
-compuerta= int(input("Elija que funcion desea analizar: \n 1- Funcion AND\n 2- Funcion OR\n 3- Funcion NOT\n 4- Funcion NAND\n 5- Funcion NOR \n 6- Funcion NOR\n Su eleccion: "))
+compuerta = pedir_compuerta()
 
-## Segun lo introducido por el usuario se iniciara alguna de las funciones y se imprimira en pantalla el valor de verdad
+# Segun lo introducido por el usuario se iniciara alguna de las funciones y se imprimira en pantalla el valor de verdad
 
 
-## AND
+# AND
 if compuerta == 1:
-    print ("Funcion AND")
+    print("Funcion AND")
     print(f"Se evalua: {valorA}, {valorB}")
-    if funcionAnd(valorA,valorB) == True:
+    if funcionAnd(valorA, valorB) == True:
         print("Verdadero")
     else:
         print("Falso")
 
 
-## OR  
+# OR
 elif compuerta == 2:
-    print ("Funcion OR")
+    print("Funcion OR")
     print(f"Se evalua: {valorA}, {valorB}")
-    if funcionOr(valorA,valorB) == True:
+    if funcionOr(valorA, valorB) == True:
         print("Verdadero")
     else:
         print("Falso")
 
 
-## NOT
+# NOT
 elif compuerta == 3:
-    print ("Funcion NOT")
+    print("Funcion NOT")
     print(f"Se evalua: {valorA}, {valorB}")
     if funcionNot(valorA) == 0:
-        print("Falso")
+        print(f"{valorA} Falso")
     else:
-        print("Verdadero")
+        print(f"{valorA} Verdadero")
     if funcionNot(valorB) == 0:
-        print("Falso")
+        print(f"{valorB} Falso")
     else:
-        print("Verdadero")    
+        print(f"{valorB} Verdadero")
 
 
-## NAND
+# NAND
 elif compuerta == 4:
-    print ("Funcion NAND")
+    print("Funcion NAND")
     print(f"Se evalua: {valorA}, {valorB}")
-    if funcionAnd(valorA,valorB) == True:
+    if funcionAnd(valorA, valorB) == True:
         print("Falso")
     else:
         print("Verdadero")
 
 
-## NOR
+# NOR
 elif compuerta == 5:
-    print ("Funcion NOR")
+    print("Funcion NOR")
     print(f"Se evalua: {valorA}, {valorB}")
-    if funcionOr(valorA,valorB) == True:
+    if funcionOr(valorA, valorB) == True:
         print("Falso")
     else:
         print("Verdadero")
 
-## XOR
+# XOR
 elif compuerta == 6:
-    print ("Funcion XOR")
+    print("Funcion XOR")
     print(f"Se evalua: {valorA}, {valorB}")
-    if valorA != valorB :
+    if valorA != valorB:
         print("Verdadero")
     else:
         print("Falso")
